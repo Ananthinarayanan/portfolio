259c597f81e3ef74034b57cdf03ad79b6fe8389a
unknown <ananthinarayanan2005@gmail.com>
2026-07-20 21:59:18 +0530

    Fix routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Navbarport from './Port/nav';
-import Firstpg from './Port/firstpg';
-import Aboutport from './Port/about';
-import Contactport from './Port/contact';
-import Education from './Port/education';
-import Cert from './Port/cert';
-import Skillport from './Port/skill';
-
-
-
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navbarport/>
-      <BrowserRouter>
-      <Routes>
-       <Route path='/' element={<Firstpg/>}></Route>
-       <Route path='/a' element={<Aboutport/>}></Route>
-       <Route path='/b' element={<Skillport/>}></Route>
-       <Route path='/c' element={<Education/>}></Route>
-        <Route path="/certificate" element={<Cert />} />
-       <Route path='/d' element={<Contactport/>}></Route>
-      </Routes>
-      </BrowserRouter>
+      
+      <HashRouter>
+        <Navbarport />
+        <Routes>
+          <Route path="/" element={<Firstpg />} />
+          <Route path="/a" element={<Aboutport />} />
+          <Route path="/b" element={<Skillport />} />
+          <Route path="/c" element={<Education />} />
+          <Route path="/certificate" element={<Cert />} />
+          <Route path="/d" element={<Contactport />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
-
-export default App;
